879dd6dc2bbf0c3257c0f99f5efa88c4003a1c70
meir lerner <mchlerner@gmail.com>
2023-08-10 12:00:43 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,58 @@
+import { Sedra, HDate } from '@hebcal/core';
+import './style.css';
+import getText from './text';
+
+import Alpine from 'alpinejs'
+
+
+
+window.Alpine = Alpine
+
+
+const parshiyot = {
+  Bereshit: 'בראשית', Noach: 'נח', 'Lech-Lech': 'לך לך', Vayera: 'וירא', 'Chayei Sara': 'חיי שרה', Toldot: 'תולדות', Vayetzei: 'ויצא', Vayishlach: 'וישלח', Vayeshev: 'וישב', Miketz: 'מקץ',
+  Vayigash: 'ויגש', Vayechi: 'ויחי', Shemot: 'שמות', Vaera: 'וארא', Bo: 'בא', Beshalach: 'בשלח', Yitro: 'יתרו', Mishpatim: 'משפטים', Terumah: 'תרומה', Tetzaveh: 'תצוה',
+  'Ki Tisa': 'כי תשא', Vayakhel: 'ויקהל', Pekudei: 'פקודי', Vayikra: 'ויקרא', Tzav: 'צו', Shmini: 'שמיני', Tazria: 'תזריע', Metzora: 'מצורע', 'Achrei Mot': 'אחרי מות', Kedoshim: 'קדושים', Emor: 'אמור',
+  Behar: 'בהר', Bechukotai: 'בחוקותי', Bamidbar: 'במדבר', Nasso: 'נשא', 'Beha\'alotcha': 'בהעלותך', 'Sh\'lach': 'שלח', Korach: 'קורח', Chukat: 'חוקת',
+  Devarim: 'דברים', Vaetchanan: 'ואתחנן', Eikev: 'עקב', "Re'eh": 'ראה', Shoftim: 'שופטים', 'Ki Teitzei': 'כי תצא', 'Ki Tavo': 'כי תבוא', Nitzavim: 'נצבים', Vayeilech: 'וילך', 'Ha\'Azinu': 'האזינו'
+};
+
+Alpine.data('app', () => ({
+  init() {
+    if (localStorage.getItem('settings')) {
+      this.settings = JSON.parse(localStorage.getItem('settings'))
+    }
+    this.sedra = new Sedra(new HDate().getFullYear(), this.settings.location === 'israel').get(new HDate())
+    this.selectParasha = this.parashatHasavua()
+    this.getText()
+  },
+  sedra: null,
+  text: null,
+  parshiyot,
+  selectParasha: 'Vaetchanan',
+  slideOverOpen: false,
+  settings: {
+    order: 'pasuk',
+    showRasi: false,
+    preLine: false,
+    location: navigator.language.startsWith('he') ? 'israel' : 'chul'
+  },
+  heDateAndParasha() { return `${new HDate().renderGematriya(true)} פרשת ${this.parshiyot[this.parashatHasavua()]}` },
+  getHDate() { return new HDate().renderGematriya(true) },
+  getText() {
+    this.text = getText(this.selectParasha || this.parashatHasavua(), this.settings.order, this.settings.showRasi)
+  },
+  parashatHasavua() {
+    return this.sedra.length === 1 ? this.sedra[0] : `${this.sedra[0]}-${this.sedra[1]}`;
+  },
+  toggleSettings() {
+    this.slideOverOpen = !this.slideOverOpen;
+  },
+  onSettingsChangeHandler() {
+    localStorage.setItem('settings', JSON.stringify(this.settings))
+    this.getText()
+  }
+}))
+
+Alpine.start()
+
---
+++ text.js
@@ -0,0 +1,64 @@
+import parshiyot from './data/parshiyot.json' assert { type: 'json' };
+
+export default async function getText(parashat, order = 'pasuk', showRashi = false) {
+  let { chumash, start: [perekStart, pasukStart], end: [perekEnd, pasukEnd] } = parshiyot[parashat];
+  const torahText = (await import(`./data/torah/${chumash}.json`)).text;
+  const targumText = (await import(`./data/targum/${chumash}.json`)).text;
+  let rashiText = '';
+  if (showRashi) {
+    rashiText = (await import(`./data/rashi/${chumash}.json`)).text;
+  }
+  let text = '';
+  let parasha = '';
+  let parashaTrgum = '';
+  let parashaRashi = '';
+  while (perekEnd > perekStart || (pasukEnd >= pasukStart && perekEnd === perekStart)) {
+    const torah = torahText[perekStart][pasukStart];
+    const targum = targumText[perekStart][pasukStart];
+    const rashi = rashiText?.[perekStart]?.[pasukStart];
+    const [pasuk, perek] = getPasukAndPerekHe(pasukStart, perekStart);
+    if (order === 'pasuk') {
+      text += `<div>${perek ? `<span class="perek">${perek}</span>` : ''} <span class="pasuk">${pasuk}</span> <span class="torah">${torah}
+      ${torah}</span>
+      <span class="targum">${targum}</span>${rashi && showRashi ? `
+      <span class="rashi">${rashi}</span>` : ''}</div>`;
+    } else {
+      parasha += `${perek ? `<span class="perek">${perek}</span>` : ''} <span class="pasuk">${pasuk}</span> ${torah} `;
+      parashaTrgum += `${perek ? `<span class="perek">${perek}</span>` : ''} <span class="pasuk">${pasuk}</span> ${targum} `;
+      if (rashi) {
+        parashaRashi += `${perek ? `<span class="perek">${perek}</span>` : ''} <span class="pasuk">${pasuk}</span> ${rashi} `;
+      }
+      if (torah.endsWith('(ס)') || torah.endsWith('(פ)') || (pasukStart === pasukEnd && perekStart === perekEnd)) {
+        text += `<div><span class="torah">${parasha}</span>  <span class="torah">${parasha}</span> <span class="targum">${parashaTrgum}</span>${parashaRashi && showRashi ? `<span class="rashi">${parashaRashi}</span>` : ''}</div>`;
+        parasha = '';
+        parashaTrgum = '';
+        parashaRashi = '';
+      }
+    }
+    if (torahText[perekStart].length - 1 > pasukStart) {
+      pasukStart++;
+    } else {
+      pasukStart = 0;
+      perekStart++;
+    }
+  }
+  return text;
+}
+
+function getPasukAndPerekHe(pasuk, perek) {
+  const hebrewLetters = [
+    "א", "ב", "ג", "ד", "ה", "ו", "ז", "ח", "ט", "י", "יא",
+    "יב", "יג", "יד", "טו", "טז", "יז", "יח", "יט", "כ", "כא",
+    "כב", "כג", "כד", "כה", "כו", "כז", "כח", "כט", "ל", "לא",
+    "לב", "לג", "לד", "לה", "לו", "לז", "לח", "לט", "מ", "מא",
+    "מב", "מג", "מד", "מה", "מו", "מז", "מח", "מט", "נ", "נא",
+    "נב", "נג", "נד", "נה", "נו", "נז", "נח", "נט", "ס", "סא",
+    "סב", "סג", "סד", "סה", "סו", "סז", "סח", "סט", "ע", "עא",
+    "עב", "עג", "עד", "עה", "עו", "עז", "עח", "עט", "פ", "פא",
+    "פב", "פג", "פד", "פה", "פו", "פז", "פח", "פט", "צ", "צא",
+    "צב", "צג", "צד", "צה", "צו", "צז", "צח", "צט", "ק", "קא",
+    "קב", "קג", "קד", "קה", "קו", "קז", "קח", "קט"
+  ];
+  return [hebrewLetters[pasuk], pasuk === 0 ? hebrewLetters[perek] : '']
+}
+
