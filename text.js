2dddfbebc059eeaa38ec7c6209f523f2b1ef4106
meir lerner <mchlerner@gmail.com>
2023-08-15 01:39:11 +0300

    description

--- text.js
+++ text.js
@@ -1,11 +1,5 @@
 import parshiyot from './data/parshiyot.json' assert { type: 'json' };
 
-
-const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-<path d="M13.225 18.412a1.595 1.595 0 0 1 -1.225 .588c-.468 0 -.914 -.214 -1.225 -.588l-4.361 -5.248a1.844 1.844 0 0 1 0 -2.328l4.361 -5.248a1.595 1.595 0 0 1 1.225 -.588c.468 0 .914 .214 1.225 .588l4.361 5.248a1.844 1.844 0 0 1 0 2.328l-4.361 5.248z"></path>
-</svg>`
-
 export default async function getText(parashat, order = 'pasuk', showRashi = false) {
   let { chumash, start: [perekStart, pasukStart], end: [perekEnd, pasukEnd], aliyot } = parshiyot[parashat];
   const torahText = (await import(`./data/torah/${chumash}.json`)).text;
