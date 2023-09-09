002a1b4ffbc57586e80068940629d678798e3387
liy <liy@liy.ovh>
2023-09-09 22:43:10 +0300

    liy update

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 import { Sedra, HDate } from '@hebcal/core';
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
 
 import getText from './text';
 import { getParshiyotList, trnslateParshiyot } from './parshiyot';
 import './style.css';
 
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
 
 Alpine.data('app', () => ({
   async init() {
     if (localStorage.getItem('settings')) {
-      this.settings = JSON.parse(localStorage.getItem('settings'))
+      this.settings = JSON.parse(localStorage.getItem('settings'));
     }
-    this.parshiyotList = getParshiyotList()
-    this.selectParasha = trnslateParshiyot(new Sedra(new HDate().getFullYear(), this.settings.location === 'israel').get(new HDate()))
-    this.parashatHashavua = this.selectParasha
+    this.parshiyotList = getParshiyotList();
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('p')) {
+      this.parashatHashavua = trnslateParshiyot(new Sedra(new HDate().getFullYear(), this.settings.location === 'israel').get(new HDate()));
+      this.selectParasha = urlParams.get('p');
+    } else {
+      this.selectParasha = trnslateParshiyot(new Sedra(new HDate().getFullYear(), this.settings.location === 'israel').get(new HDate()));
+      this.parashatHashavua = this.selectParasha;
+    }
     await this.getText()
     window.addEventListener('scroll', (e) => {
-      this.currentAliya = [1, 2, 3, 4, 5, 6].findLast(n => (document.getElementById(`aliya-${n}`).getBoundingClientRect().top + (window.innerHeight - 75)) < window.innerHeight) || 0
+      this.currentAliya = [1, 2, 3, 4, 5, 6].findLast(n => (document.getElementById(`aliya-${n}`).getBoundingClientRect().top + (window.innerHeight - 75)) < window.innerHeight) || 0;
     })
+    document.title = `${this.selectParasha} | שניים מקרא ואחד תרגום`;
   },
   parshiyotList: null,
   parashatHashavua: null,
@@ -35,7 +42,9 @@ Alpine.data('app', () => ({
     fontSize: 18,
     aliyaByDay: false
   },
-  heDateAndParasha() { return `${new HDate().renderGematriya(true)} פרשת ${this.parashatHashavua}` },
+  heDateAndParasha() {
+    return `${new HDate().renderGematriya(true)} פרשת ${this.parashatHashavua}`;
+  },
   async getText() {
     this.text = await getText(this.selectParasha || this.parashatHashavua(), this.settings.order, this.settings.showRasi)
     if (new Date().getDay() && this.settings.aliyaByDay && this.parashatHashavua === this.selectParasha) {
@@ -43,6 +52,12 @@ Alpine.data('app', () => ({
         this.scrollToAliya(new Date().getDay(), 'instant')
       }, 1000);
     }
+    if (history.pushState) {
+      const base = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
+      const newurl = `${base}?p=${this.selectParasha}`;
+      window.history.pushState({ path: newurl }, '', newurl);
+    }
+    document.title = `${this.selectParasha} | שניים מקרא ואחד תרגום`;
   },
   toggleSettings() {
     this.slideOverOpen = !this.slideOverOpen;
@@ -71,4 +86,3 @@ Alpine.data('app', () => ({
 }))
 
 Alpine.start()
-
